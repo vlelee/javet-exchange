9c7b35429ace6c524a980d3e759078e8e6e76ba9
Abnob Doss <abanoubdoss@gmail.com>
2018-07-31 09:32:22 -0400

    Readded JMS

--- src/main/java/com/citi/exchange/ExchangeApplication.java
+++ src/main/java/com/citi/exchange/ExchangeApplication.java
@@ -1,29 +1,29 @@
 package com.citi.exchange;
 //
-//import com.citi.exchange.algorithms.TradeExecution;
+import com.citi.exchange.algorithms.TradeExecution;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-//import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-//import org.springframework.jms.annotation.EnableJms;
-//import org.springframework.jms.config.JmsListenerContainerFactory;
-//import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
-//import org.springframework.jms.core.JmsTemplate;
-//import org.springframework.jms.core.MessageCreator;
-//import org.springframework.util.FileSystemUtils;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-//import javax.jms.ConnectionFactory;
-//import javax.jms.JMSException;
-//import javax.jms.Message;
-//import javax.jms.Session;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.io.File;
 
 @EntityScan("com.citi.exchange.entities")
@@ -33,18 +33,19 @@ import java.io.File;
 //@EnableJms
 public class ExchangeApplication extends WebMvcConfigurerAdapter {
 
+
     public static void main(String[] args) {
         SpringApplication.run(com.citi.exchange.ExchangeApplication.class, args);
 
     }
 
-//	@Bean
-//		// Strictly speaking this bean is not necessary as boot creates a default
-//	JmsListenerContainerFactory<?> myJmsContainerFactory(ConnectionFactory connectionFactory) {
-//		SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
-//		factory.setConnectionFactory(connectionFactory);
-//		return factory;
-//	}
+	@Bean
+		// Strictly speaking this bean is not necessary as boot creates a default
+	JmsListenerContainerFactory<?> myJmsContainerFactory(ConnectionFactory connectionFactory) {
+		SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory);
+		return factory;
+	}
 //		// Clean out any ActiveMQ data from a previous run
 //		FileSystemUtils.deleteRecursively(new File("activemq-data"));
 //
