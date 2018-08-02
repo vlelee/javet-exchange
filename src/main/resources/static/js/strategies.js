fbf6ad1b5100b2c8e38cb3fb6693ab014e235fca
Abnob Doss <abanoubdoss@gmail.com>
2018-08-02 15:29:17 -0400

    Hide strategies that become inactive

--- src/main/resources/static/js/strategies.js
+++ src/main/resources/static/js/strategies.js
@@ -52,6 +52,10 @@ function loadStrategies() {
                     $.get(`http://localhost:8082/api/strategies/${strategy.id}/position`, function(strategy_position) {
                         $(`#strategy${strategy.id}-next-position`).text(strategy_position)
                     });
+                    $.get(`http://localhost:8082/api/strategies/${strategy.id}`, function(strategy) {
+                        if(!strategy.active)
+                            window.location.reload(true);
+                    });
                 }, 5000);                
             }
         });
