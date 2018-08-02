799f0decc584f359a44c724875f4ce56848cfca6
jrose023 <jar892@nyu.edu>
2018-08-02 15:13:10 +0000

    rewrite sql script for prod support, taking out create database and adding precision to timestamp

--- runscript.sql
+++ runscript.sql
@@ -1,7 +1,7 @@
- drop database javet;
-
-CREATE DATABASE IF NOT EXISTS javet;
-use javet;
+--  drop database javet;
+--
+-- CREATE DATABASE IF NOT EXISTS javet;
+-- use javet;
 
 create table stocks (
 ticker varchar(10) primary key unique,
@@ -13,8 +13,8 @@ id int primary key auto_increment,
 strategy_name varchar(30) unique not null,
 algo enum('TMA', 'BB','PB') not null,
 stock varchar(10) not null,
-start_time timestamp not null,
-end_time timestamp null default null ,
+start_time timestamp(6) not null,
+end_time timestamp(6) null default null ,
 initiation_price double not null,
 exit_price double,
 num_shares int not null,
@@ -28,19 +28,19 @@ FOREIGN KEY (stock) REFERENCES stocks(ticker) on delete restrict
 CREATE TABLE stock_prices(
 id int primary key auto_increment,
 stock varchar(10) not null,
-time_stamp timestamp not null ,
+time_stamp timestamp(6) not null ,
 price double not null,
 FOREIGN KEY (stock) REFERENCES stocks(ticker) on delete restrict
 );
 
 create table trades(
-id int primary key auto_increment, 
+id int primary key auto_increment,
 stock varchar(10) not null,
 strategy int not null,
 buying boolean not null,
 num_shares int not null,
 trade_price double not null,
-time_traded timestamp default current_timestamp,
+time_traded timestamp(6) default current_timestamp(6),
 response_message varchar(1000),
 FOREIGN KEY (stock) REFERENCES stocks(ticker) on delete restrict,
 FOREIGN KEY (strategy) REFERENCES strategy_configurations(id) on delete restrict
@@ -48,10 +48,11 @@ FOREIGN KEY (strategy) REFERENCES strategy_configurations(id) on delete restrict
 
 
 insert into stocks values ("AAPL", "Apple Inc.");
-insert into stocks values ("GOOGL", "Alphabet Inc.");
-insert into stocks values ("C ", "Citi");
-insert into stocks values ("BFY ", "Blackrock");
-insert into strategy_configurations values (1, "Julie 3rdQ MSFT", 'TMA', "GOOGL", current_timestamp, null, 44, null, 100,  true, true, 10.5, 5.5);
-insert into strategy_configurations values (2, "2ndStrat", 'BB', "GOOGL", current_timestamp, null, 100.50, null, 100,  true, false, 10.5, 5.5);
-insert into strategy_configurations values (3, "Abe 3rdQ GOOGL", 'TMA', "GOOGL", current_timestamp, null, 100.50, null, 100,  true, true, 10.5, 5.5);
-insert into strategy_configurations values (4, "Vicki 3rdQ APPL", 'TMA', "AAPL", current_timestamp, null, 100.50, null, 100,  true, true, 10.5, 5.5);
+insert into stocks values ("GOOG", "Alphabet Inc.");
+insert into stocks values ("MSFT", "Microsoft");
+insert into stocks values ("NSC", "Norfolk Southern Corp.");
+insert into stocks values ("BRK-A", "Berkshire Hathaway Inc., Class A");
+-- insert into strategy_configurations values (1, "Julie 3rdQ MSFT", 'TMA', "GOOGL", current_timestamp, null, 44, null, 100,  true, true, 10.5, 5.5);
+-- insert into strategy_configurations values (2, "2ndStrat", 'BB', "GOOGL", current_timestamp, null, 100.50, null, 100,  true, false, 10.5, 5.5);
+-- insert into strategy_configurations values (3, "Abe 3rdQ GOOGL", 'TMA', "GOOGL", current_timestamp, null, 100.50, null, 100,  true, true, 10.5, 5.5);
+-- insert into strategy_configurations values (4, "Vicki 3rdQ APPL", 'TMA', "AAPL", current_timestamp, null, 100.50, null, 100,  true, true, 10.5, 5.5);
